9611be820070f6278bb0d5a1ccdf6755d75ab22f
patthaveekarn <patthaveekarn2544@gmail.com>
2023-11-23 22:23:51 +0700

    change hello

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+//import java.util.Scanner;   //Import
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello welcome!");
+//        int n;
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
